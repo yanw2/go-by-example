1230c8ba3d2eff26b06fa157766600af9b59a375
yanw2 <wenjing.yan@anz.com>
2020-06-24 21:55:00 +1000

    Add string-functions

---
+++ string-functions/string-functions.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	s "strings"
+)
+
+// We alias fmt.Println to a shorter name.
+var p = fmt.Println
+
+func main() {
+	// We need to pass the string in question as the first argument to the function.
+	p("Contains:  ", s.Contains("test", "es"))
+	p("Count:     ", s.Count("test", "t"))
+	p("HasPrefix: ", s.HasPrefix("test", "te"))
+	p("HasSuffix: ", s.HasSuffix("test", "st"))
+	p("Index:     ", s.Index("test", "e"))
+	p("Join:      ", s.Join([]string{"a", "b"}, "-"))
+	p("Repeat:    ", s.Repeat("a", 5))
+	p("Replace:   ", s.Replace("foo", "o", "0", -1))
+	p("Replace:   ", s.Replace("foo", "o", "0", 1))
+	p("Split:     ", s.Split("a-b-c-d-e", "-"))
+	p("ToLower:   ", s.ToLower("TEST"))
+	p("ToUpper:   ", s.ToUpper("test"))
+	p()
+
+	p("Len: ", len("hello"))
+	p("Char:", "hello"[1])
+}
